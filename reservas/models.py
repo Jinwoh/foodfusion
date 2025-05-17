5ff442a83027985a131ffd622e61be0dd271f020
Alex <9929parra@gmail.com>
2025-05-17 02:50:13 -0300

    Settings

--- reservas/models.py
+++ reservas/models.py
@@ -1,7 +1,10 @@
 from django.db import models  # type: ignore
 from django.contrib.auth.models import User  # type: ignore
-from django.utils import timezone
-
+from django.db import models  # type: ignore
+from django.contrib.auth import get_user_model
+from django.db import models  # type: ignore
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 class CategoriaMenu(models.Model):
     nombre = models.CharField(max_length=50, unique=True)
@@ -9,21 +12,19 @@ class CategoriaMenu(models.Model):
     def __str__(self):
         return self.nombre
 
-
 class Menu(models.Model):
     nombre = models.CharField(max_length=100)
     categoria = models.ForeignKey('CategoriaMenu', on_delete=models.CASCADE)
     precio = models.DecimalField(max_digits=8, decimal_places=2)
     stock = models.PositiveIntegerField()
     disponible = models.BooleanField(default=True)
-    img_url = models.ImageField(upload_to='menus/', null=True, blank=True)  # 👈 nuevo campo
+    img_url = models.ImageField(upload_to='menus/', null=True, blank=True)
 
     def __str__(self):
         return f"{self.nombre} - {self.precio} Gs"
 
-
 class Cliente(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
     nombre_apellido = models.CharField(max_length=100)
     cedula = models.CharField(max_length=20, unique=True)
     correo = models.EmailField(unique=True)
@@ -32,11 +33,9 @@ class Cliente(models.Model):
     def __str__(self):
         return f"{self.nombre_apellido} ({self.cedula})"
 
-
 class Mesa(models.Model):
     numero = models.PositiveBigIntegerField(unique=True, null=True, blank=True)
     capacidad = models.PositiveIntegerField()
-    
     ESTADO_CHOICES = [
         ('disponible', 'Disponible'),
         ('reservada', 'Reservada'),
@@ -47,19 +46,17 @@ class Mesa(models.Model):
     def __str__(self):
         return f"Mesa #{self.numero or self.id} - {self.estado}"
 
-
 class Reserva(models.Model):
     fecha_inicio = models.DateTimeField()
     fecha_fin = models.DateTimeField()
-
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     mesa = models.ForeignKey(Mesa, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Reserva de {self.cliente.nombre_apellido} desde {self.fecha_inicio} hasta {self.fecha_fin}"
 
-
 class Empleado(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
     nombre_apellido = models.CharField(max_length=100)
     cedula = models.CharField(max_length=20, unique=True)
     celular = models.CharField(max_length=20)
